eedb848e4861e7ebcd712c6f5c9922da87eb50a9
MKS-students <mks.students@hackreactor.com>
2017-03-07 14:59:15 -0500

    Finished communication with my back and front-end

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,10 +2,47 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('formCtrl',['$scope','$http', function($scope,$http){
 
+  // $http.get('/customers').then(function(res){
+  //   console.log('got the data requested', res)
+  //   $scope.customers = res.data;
+  // });
+  $scope.addClient = function(){
+    console.log($scope.contact)
+    console.log("here")
+    $http.post('/customers', $scope.contact).then(function(){
+      console.log("here")
+      $http.get('/customers').then(function(res){
+        console.log("THIS IS THE GET DATA FROM CONTROLLER", res.data)
+        $scope.customers = res.data;
+      })
+    })
+  }
+
+
+
+  // person1 = {
+  //   name: "Sally Sue",
+  //   isMilitary: "YES",
+  //   phoneNumber: "(757) 597-6656"
+  // };
+  //
+  // person2 = {
+  //   name: "John Doe",
+  //   isMilitary: "NO",
+  //   phoneNumber: "(757) 597-6676"
+  // };
+  //
+  // person3 = {
+  //   name: "John Smith",
+  //   isMilitary: "YES",
+  //   phoneNumber: "(757) 546-8756"
+  // };
+  //
+  // var customers = [person1,person2,person3];
   $http.get('/customers').then(function(res){
-    console.log('got the data requested', res)
+    console.log("THIS IS THE GET DATA FROM CONTROLLER", res.data)
     $scope.customers = res.data;
-  });
+  })
 
 
 
--- server.js
+++ server.js
@@ -1,13 +1,59 @@
 var express = require('express');
 var app = express();
-
-// app.get('/',function(req,res){
-//   res.send('Hello World')
-// })
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://mvp2017:mvp123@ds025439.mlab.com:25439/newcustomerdatabase');
 
 
+
+var customersSchema = new Schema({name: String,
+                          isMilitary: String,
+                          phoneNumber: String})
+
+var Customer = mongoose.model('Customer', customersSchema)
+
+// var newCustomer = new Customer({
+//   name: "Bryan",
+//   isMilitary: "Yes",
+//   phoneNumber: "(111) 111-1111"
+// })
+//
+// newCustomer.save(function(err){
+//   if (err) throw err;
+//   console.log("new customer saved")
+// });
+
+app.post('/customers',function(req,res){
+  console.log('this is the express POST==>',req.body)
+  var newCustomer = new Customer({
+    name: req.body.Name,
+    isMilitary: req.body.isMilitary,
+    phoneNumber: req.body.phoneNumber
+  })
+  newCustomer.save(function(err){
+    if (err) throw err;
+    console.log('new customer saved')
+  })
+  res.status(200).send()
+})
+
+app.get('/customers',function(req,res){
+  Customer.find({},function(err, customers){
+    if (err) throw err;
+    res.json(customers)
+    //$scope.customers = customers;
+  })
+})
+
+
+// app.post('/customers',function(req,res){
+//   console.log(req.body);
+// })
 
 app.listen(3000);
 console.log("we are up and running")
